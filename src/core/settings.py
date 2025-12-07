ea2eb79f3fb792059b9509fcf649f3ae2a720dfb
LeomaxFilho <leomax.filho@gmail.com>
2025-12-07 15:06:10 -0300

    add: database settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-pgvector',
+    'pgvector',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '_',
+        'USER': '_',
+        'PASSWORD': '_',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
