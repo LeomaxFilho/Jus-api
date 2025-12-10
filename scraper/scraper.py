97e2b85dc1a727eb87440bc56cf50d01e889b680
LeomaxFilho <leomax.filho@gmail.com>
2025-12-10 19:27:35 -0300

    add: law crawler start

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,15 +2,20 @@
 law scraper
 """
 
+import asyncio
 
-def main():
-    """
-    main function
-    """
+from utils.functions import fetch_laws, save_json
+
+
+async def main():
+    """main function"""
+    url = 'https://www.planalto.gov.br/ccivil_03/constituicao/constituicao.htm'
+    response = await fetch_laws(url)
+    save_json(response, __file__)
 
 
 ...
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
--- scraper/utils/functions.py
+++ scraper/utils/functions.py
@@ -2,20 +2,24 @@
 function utils
 """
 
-import json
+import os
 
 import aiohttp
-from aiohttp.http import RESPONSES
 
 
 async def fetch_laws(url: str) -> str:
+    header = {
+        'User-Agent': '"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"'
+    }
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as request:
-            response = await request.text()
+        async with session.get(url, headers=header) as request:
+            response = await request.text(encoding='ISO-8859-1')
 
     return response
 
 
 def save_json(data: str, path: str):
-    with open(f'{path}/../data/law.json') as file:
-        json.dump(data, file, indent=4, ensure_ascii=False)
+    with open(
+        f'{os.path.dirname(os.path.abspath(path))}/data/law.html', 'w', encoding='ISO-8859-1'
+    ) as file:
+        _ = file.write(data)
