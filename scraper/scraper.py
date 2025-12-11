457747049eeeea4e82d31b934f7321db5632f2aa
LeomaxFilho <leomax.filho@gmail.com>
2025-12-10 22:49:50 -0300

    add: laws scraper to txt

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,17 +4,16 @@ law scraper
 
 import asyncio
 
-from utils.functions import fetch_laws, save_json
+from utils.functions import fetch_laws, save_json, soup_laws
 
 
 async def main():
     """main function"""
     url = 'https://www.planalto.gov.br/ccivil_03/constituicao/constituicao.htm'
-    response = await fetch_laws(url)
-    save_json(response, __file__)
+    response: str = await fetch_laws(url)
+    laws: str = await soup_laws(response)
 
-
-...
+    save_json(laws, __file__)
 
 
 if __name__ == '__main__':
--- scraper/utils/functions.py
+++ scraper/utils/functions.py
@@ -3,11 +3,14 @@ function utils
 """
 
 import os
+from pickletools import string1
 
 import aiohttp
+from bs4 import BeautifulSoup
 
 
 async def fetch_laws(url: str) -> str:
+    """fetch data from url"""
     header = {
         'User-Agent': '"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"'
     }
@@ -18,8 +21,19 @@ async def fetch_laws(url: str) -> str:
     return response
 
 
-def save_json(data: str, path: str):
+def save_json(data: str, path: str) -> None:
+    """save file to json"""
+
     with open(
-        f'{os.path.dirname(os.path.abspath(path))}/data/law.html', 'w', encoding='ISO-8859-1'
+        f'{os.path.dirname(os.path.abspath(path))}/data/law.txt', 'w', encoding='utf-8'
     ) as file:
         _ = file.write(data)
+
+
+async def soup_laws(html: str) -> str:
+    soup = BeautifulSoup(html, 'html.parser')
+
+    content = soup.find_all('p')
+    real_content = '\n\n'.join([c.getText(strip=True) for c in content])
+
+    return real_content
